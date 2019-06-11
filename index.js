cd049cbc6a57f124e3d008809a9bcbfb72d9f59a
SparrowHawk Shadd <shadrackronoh67@gmail.com>
2019-06-11 12:54:07 +0300

    new commit

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mysql = require("mysql")
 const app = express();
+const pool = mysql.createPool(
+    {
+        host: "localhost",
+        user: "foo",
+        password: "password",
+        database: "sparrow_school",
+        insecureAuth: true
+    }
+);
 
-app.get("/", function (req, res){
-//logic
-res.send("hallo sparrow")
+app.get("/api/schools", (req, res) => {
+    pool.query("SELECT reg_number, namme FROM student", (error, rows) => {
+        if (error){
+            return res.status(500).json({ error });
+        }
+
+        res.json(rows);
+    });
 });
+
 app.listen(8080, function () {
     console.log("app listening on port 8080")
 });
