98228d50475a9c9e02f0296e297ee44d5745ba5e
SparrowHawk Shadd <shadrackronoh67@gmail.com>
2019-07-05 08:42:11 +0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
+import highschool from "./highschool";
 import "./App.css";
 
 function App() {
---
+++ client/src/highschool.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function highschools({highschooldisplay}){
+    const {id, namee, form, reg_id } = highschooldisplay;
+
+    
+}
+
+export default highschools;
