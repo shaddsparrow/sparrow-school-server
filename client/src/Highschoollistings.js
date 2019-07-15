bb978407dd5e9d3df7fe5e859b69bff2e6455e1c
SparrowHawk Shadd <shadrackronoh67@gmail.com>
2019-07-15 21:22:58 +0300

    call from API

--- client/src/Highschoollistings.js
+++ client/src/Highschoollistings.js
@@ -1,20 +1,61 @@
 import React from "react";
-
+import axios from "axios";
 import Highschool from "./Highschool";
 
-function Highschoollistings({highschooldisplay}){
-    return(
-        <div className="ss-container">
-            <div className="ss-highschool-listings">
-                {
-                    highschooldisplay.map(m => (
-                        <Highschool key={m.id} highschooldisplay={m} />
-                    ))
-                }
-            </div>
-        </div>
-    );
+class Highschoollistings extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            highschooldisplay: [],
+            loading: false,
+            error: false,
+        }
+    }
+
+    componentDidMount(){
+        this.fetchparentdisplay();
+    }
+
+    fetchparentdisplay() {
+        this.setState({ loading: true, error: false })
+        axios.get("/api/schools/:high")
+            .then(response => {
+                this.setState({
+                    highschooldisplay: response.data,
+                    loading: false,
+                    error: false
+                });
+            })
+            .catch(
+            error => {
+                this.setState({
+                    highschooldisplay: [],
+                loading:false,
+                error:true
+                })
+                        
+            });
+    }
+
+    render() {
+        const {highschooldisplay} = this.state
+
+        return (
+            <div className="ss-container" >
+                <div className="ss-highschool-listings">
+                    {
+                        highschooldisplay.map(m => (
+                            <Highschool key={m.id} highschooldisplay={m} />
+                        ))
+                    }
+                </div>
+            </div>
+        );
+
+    }
 }
+
 
 export default Highschoollistings;
