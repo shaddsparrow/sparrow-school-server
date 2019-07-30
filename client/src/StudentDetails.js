981220dafab8c50cacb3c904d04330dbcd7f42a3
SparrowHawk Shadd <shadrackronoh67@gmail.com>
2019-07-30 09:50:03 +0300

    display students details

--- client/src/StudentDetails.js
+++ client/src/StudentDetails.js
@@ -1,11 +1,56 @@
 import React from "react";
+import {Component} from "react";
+import Student from "./Student";
+import Axios from "axios";
 
-function StudentDetails(props) {
-    return (
+class StudentDetails extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            studentdisplay:[],
+            loading:false,
+            error:false
+        }
+    }
+
+    componentDidMount(){
+        this.fetchstudentdetails();
+    }
+
+    fetchstudentdetails(){
+        this.setState({loading:true,error:false});
+
+        Axios.get("/api/student")
+        .then(response=>{
+            this.setState({
+                studentdisplay:response.data,
+                loading:false,
+                error:false
+            })
+        }).catch(error=>{
+            this.setState({
+                studentdisplay:[],
+                loading:false,
+                error:true
+            })
+        })
+    }
+render(){
+    const{studentdisplay}=this.state;
+
+    return(
         <div>
-            <h1>Movie {props.studentId} Details</h1>
+            {studentdisplay.map(student=>(
+                <Student key={student.id} studentdisplay={student}/>
+            ))}
         </div>
-    );
+    )
 }
+
+
+} 
+
+
 
 export default StudentDetails;
